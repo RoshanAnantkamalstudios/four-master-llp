3629271d1b6ffdb471f7563c2afa437f885e8dda
RoshanAnantkamalstudios <roshan.anantkamalstudios@gmail.com>
2026-02-02 13:26:53 +0530

    updates

---
+++ frontend/src/index.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomeV1 from "./components/home";
+import ProptertyDetails from "./components/property-details";
+import ProptertyGrid from "./components/property-grid";
+import Propterty from "./components/property";
+import About from "./components/about";
+import Team from "./components/team";
+import SignIn from "./components/sign-in";
+import SignUp from "./components/sign-up";
+import AddProperty from "./components/add-property";
+import Contact from "./components/contact";
+import Blog from "./components/blog";
+import BlogDetails from "./components/blog-details";
+
+class Root extends Component {
+  render() {
+    return (
+      <Router basename="/">
+        <div>
+          <Switch>
+            <Route exact path="/" component={HomeV1} />
+            <Route path="/property-details" component={ProptertyDetails} />
+            <Route path="/property-grid" component={ProptertyGrid} />
+            <Route path="/property" component={Propterty} />
+            <Route path="/about" component={About} />
+            <Route path="/team" component={Team} />
+            <Route path="/sign-in" component={SignIn} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route path="/add-property" component={AddProperty} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/blog-details" component={BlogDetails} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default Root;
+
+ReactDOM.render(<Root />, document.getElementById("mingrand"));
